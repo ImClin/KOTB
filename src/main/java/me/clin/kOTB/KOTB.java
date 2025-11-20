0f97573c90b99e2340a98494688e18859885a862
Colin Canters <colincanters@gmail.com>
2025-11-20 18:01:23 +0100

    King of the Bridge init release

---
+++ src/main/java/me/clin/kOTB/KOTB.java
@@ -0,0 +1,17 @@
+package me.clin.kOTB;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KOTB extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
